7c8c4d13a36484ea48082e28ac44ca3658048589
Nick <nickcox61@gmail.com>
2026-04-11 19:10:04 -0400

    Add mathjax test coulomb

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeMathjax from 'rehype-mathjax';
 
-// https://astro.build/config
 export default defineConfig({
-    markdown: {
-        remarkPlugins: [remarkMath],
-        rehypePlugins: [rehypeMathjax],
-    },  
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [rehypeMathjax, {
+        tex: {
+          packages: ['base', 'ams', 'newcommand', 'autoload', 'require', 'color', 'html', 'textcomp', 'cleveref'],
+          tags: 'ams',
+        }
+      }]
+    ],
+  },
 });
